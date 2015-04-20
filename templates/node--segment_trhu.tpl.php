84e6bc2971e86feb631ce48c2518b346e1142914
reznimic <mich.reznicek@gmail.com>
2015-04-20 10:25:21 +0200

    Segment trhu, po chaosu s prehravanim

--- templates/node--segment_trhu.tpl.php
+++ templates/node--segment_trhu.tpl.php
@@ -170,10 +170,22 @@ $polezemi = country_get_list();
                             </h1>
 
                             <div class="m-item--description">
-                                <p><?= $reference['entity']->field_reference_text[$node->language][0]['value'] ?>
-                                    &rarr;</p>
+                                <?php
+
+                                ?>
+
+
+                                <p><?php if(isset($reference['entity']->field_reference_perex[$node->language][0]['value'])){
+                                        echo $reference['entity']->field_reference_perex[$node->language][0]['value'];
+                                    }else{
+                                        echo truncate($reference['entity']->field_reference_text[$node->language][0]['value']);
+                                    }
+  
+                                    ?>
+                                    <br />
+                                   <a href="<?= $GLOBALS['base_url'] ?>/node/<?= $reference['entity']->nid ?>">Detail reference</a> &rarr; [ <a href="<?= $GLOBALS['base_url'] ?>/node/<?= $reference['entity']->nid ?>/edit">editovat</a> ]</p>
                             </div>
-                            <a href="<?= $GLOBALS['base_url'] ?>/node/<?= $reference['entity']->nid ?>/edit">editovat</a>
+
                         </div>
 
                         <div class="m-properties">
@@ -274,7 +286,7 @@ if(isset($node->field_segment_faq['und'])){
     <div class="row">
         <footer class="m-section--footer">
             <div class="l-half">
-                <div class="m-section--top"><a href="">Nahoru &uarr;</a></div>
+                <div class="m-section--top"><a href="">Nahoru <i class="fa fa-arrow-up"></i></a></div>
             </div>
             <div class="l-half">
                 <ul class="m-section--nav inline-right">
@@ -289,10 +301,7 @@ if(isset($node->field_segment_faq['und'])){
 <?php
 }
 ?>
-<?php
-$block = module_invoke('views', 'block_view', 'segmentreference-block');
-print render($block);
-?>
+
 
 
 
@@ -306,164 +315,16 @@ print render($block);
     <div class="row">
         <div class="l-section--content slider block-feed-slider">
             <?php
-            for($i=0;$i<=3;$i++){
+            $block = module_invoke('views', 'block_view', 'segmentreference-block');
+            print render($block);
             ?>
-
-            <div class="blok-feed-slide">
-                <div class="m-card_thumb mm-has-hover l-double">
-
-                    <article class="m-story">
-                        <header>
-                            <div class="m-item--image" style="background-image: url(http://lorempixel.com/300/300)">
-                                <a href="">
-                                    <img src="http://lorempixel.com/400/600" alt="" />
-                                </a>
-                            </div>
-                            <div class="m-item--summary">
-                                <h1 class="m-item--hed">
-                                    <a href="">titulek titulek</a>
-                                </h1>
-
-                                <div class="m-item--description">
-                                    <p>Morbi vulputate ac quam quis feugiat. Mauris dapibus dictum ex, at sodales mauris rutrum vel. Suspendisse vestibulum enim sit amet ultricies hendrerit.</p>
-                                </div>
-                            </div>
-                            <div class="m-card--more">&rarr;</div>
-                        </header>
-                    </article>
-
-                </div>
-
-                <div class="m-card_thumb  mm-has-hover l-single">
-
-                    <article class="m-story">
-                        <header>
-                            <div class="m-item--image" style="background-image: url(http://lorempixel.com/300/300)">
-                                <a href="">
-                                    <img src="http://lorempixel.com/400/600" alt="" />
-                                </a>
-                            </div>
-                            <div class="m-item--summary">
-                                <h1 class="m-item--hed">
-                                    <a href="">titulek titulek</a>
-                                </h1>
-
-                                <div class="m-item--description">
-                                    <p>Morbi vulputate ac quam quis feugiat. Mauris dapibus dictum ex, at sodales mauris rutrum vel. Suspendisse vestibulum enim sit amet ultricies hendrerit.</p>
-                                </div>
-                            </div>
-                            <div class="m-card--more">&rarr;</div>
-                        </header>
-                    </article>
-
-                </div>
-
-                <div class="m-card_thumb  mm-has-hover l-single">
-
-                    <article class="m-story">
-                        <header>
-                            <div class="m-item--image" style="background-image: url(http://lorempixel.com/300/300)">
-                                <a href="">
-                                    <img src="http://lorempixel.com/400/600" alt="" />
-                                </a>
-                            </div>
-                            <div class="m-item--summary">
-                                <h1 class="m-item--hed">
-                                    <a href="">titulek titulek</a>
-                                </h1>
-
-                                <div class="m-item--description">
-                                    <p>Morbi vulputate ac quam quis feugiat. Mauris dapibus dictum ex, at sodales mauris rutrum vel. Suspendisse vestibulum enim sit amet ultricies hendrerit.</p>
-                                </div>
-                            </div>
-                            <div class="m-card--more">&rarr;</div>
-                        </header>
-                    </article>
-
-                </div>
-            </div>
-
-            <div class="blok-feed-slide">
-                <div class="m-card_thumb l-single">
-
-                    <article class="m-story">
-                        <header>
-                            <div class="m-item--image" style="background-image: url(http://lorempixel.com/300/300)">
-                                <a href="">
-                                    <img src="http://lorempixel.com/400/600" alt="" />
-                                </a>
-                            </div>
-                            <div class="m-item--summary">
-                                <h1 class="m-item--hed">
-                                    <a href="">titulek titulek</a>
-                                </h1>
-
-                                <div class="m-item--description">
-                                    <p>Morbi vulputate ac quam quis feugiat. Mauris dapibus dictum ex, at sodales mauris rutrum vel. Suspendisse vestibulum enim sit amet ultricies hendrerit.</p>
-                                </div>
-                            </div>
-                            <div class="m-card--more">&rarr;</div>
-                        </header>
-                    </article>
-
-                </div>
-
-                <div class="m-card_thumb l-single">
-
-                    <article class="m-story">
-                        <header>
-                            <div class="m-item--image" style="background-image: url(http://lorempixel.com/300/300)">
-                                <a href="">
-                                    <img src="http://lorempixel.com/400/600" alt="" />
-                                </a>
-                            </div>
-                            <div class="m-item--summary">
-                                <h1 class="m-item--hed">
-                                    <a href="">titulek titulek</a>
-                                </h1>
-
-                                <div class="m-item--description">
-                                    <p>Morbi vulputate ac quam quis feugiat. Mauris dapibus dictum ex, at sodales mauris rutrum vel. Suspendisse vestibulum enim sit amet ultricies hendrerit.</p>
-                                </div>
-                            </div>
-                            <div class="m-card--more">&rarr;</div>
-                        </header>
-                    </article>
-
-                </div>
-
-                <div class="m-card_thumb l-double">
-
-                    <article class="m-story">
-                        <header>
-                            <div class="m-item--image" style="background-image: url(http://lorempixel.com/300/300)">
-                                <a href="">
-                                    <img src="http://lorempixel.com/400/600" alt="" />
-                                </a>
-                            </div>
-                            <div class="m-item--summary">
-                                <h1 class="m-item--hed">
-                                    <a href="">titulek titulek</a>
-                                </h1>
-
-                                <div class="m-item--description">
-                                    <p>Morbi vulputate ac quam quis feugiat. Mauris dapibus dictum ex, at sodales mauris rutrum vel. Suspendisse vestibulum enim sit amet ultricies hendrerit.</p>
-                                </div>
-                            </div>
-                            <div class="m-card--more">&rarr;</div>
-                        </header>
-                    </article>
-
-                </div>
-            </div>
-          <?php } ?>
         </div>
     </div>
 
     <div class="row">
         <footer class="m-section--footer">
             <div class="l-half">
-                <div class="m-section--top"><a href="">Nahoru &uarr;</a></div>
+                <div class="m-section--top"><a href="">Nahoru <i class="fa fa-arrow-up"></i></a></div>
             </div>
             <div class="l-half">
                 <div class="m-section--more"><a href="">Celý archiv &rarr;</a></div>
@@ -472,6 +333,19 @@ print render($block);
     </div>
 
 </div>
+
+<?php
+function truncate($string,$length=250,$append="&hellip;") {
+$string = trim($string);
+
+  if(strlen($string) > $length) {
+      $string = wordwrap($string, $length);
+      $string = explode("\n", $string, 2);
+      $string = $string[0] . $append;
+  }
+
+  return $string;
+}
 
 
 

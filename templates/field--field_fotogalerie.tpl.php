d4228512fa2d4ed445227ba8085511fa9da89297
reznimic <mich.reznicek@gmail.com>
2015-04-23 11:51:04 +0200

    Uprava fotogalerie

--- templates/field--field_fotogalerie.tpl.php
+++ templates/field--field_fotogalerie.tpl.php
@@ -1,13 +1,6 @@
 <?php
-
-//x323-268
-
 foreach ($items AS $pole) {
     $cast = ($pole['entity']['field_collection_item'][key($pole['entity']['field_collection_item'])]['#entity']);
-
- //   dsm($cast);
-
-
 }
 ?>
 <div class="m-section">
@@ -18,35 +11,23 @@ foreach ($items AS $pole) {
             </div>
         </header>
     </div>
-
     <div class="row">
-
-
         <?php
-        foreach ($cast->field_reference_fotogalerie['und'] AS $galerie) {
+        foreach ($cast->field_reference_fotogalerie['und'] AS $galerie) { // prochazi jednotlive galerie
             ?>
             <ul class="clearing-thumbs clearing-feature" data-clearing>
                 <?php
-                foreach ($galerie['entity']->field_fotogalerie_imgs['und'] AS $poradi => $img){
+                foreach ($galerie['entity']->field_fotogalerie_imgs['und'] AS $poradi => $img){// prochazi jednotlive obrazky
                     ?>
-
                     <li class="<?=($poradi==0)?'clearing-featured-img':''?>"><a href="<?= image_style_url('none', $img['uri']) ?>"><img src="<?= image_style_url('x323-268', $img['uri']) ?>"></a></li>
-
-
                 <?php
                 }
                 ?>
-
-
             </ul>
         <?php
         }
         ?>
-
-
     </div>
-
-
     <div class="row">
         <footer class="m-section--footer"></footer>
     </div>

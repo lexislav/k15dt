3e95cb172aeb84a13475e2ba1e11a31d43e1a296
reznimic <mich.reznicek@gmail.com>
2015-04-23 11:39:35 +0200

    Uprava segmentu trhu

--- templates/node--segment_trhu.tpl.php
+++ templates/node--segment_trhu.tpl.php
@@ -182,69 +182,7 @@ $polezemi = country_get_list();
         ?>
     </div>
 </div>
-<?php
-if(isset($node->field_segment_faq['und'])){
-?>
-<div class="m-section l-feed_three">
 
-    <div class="row">
-        <header class="m-section--header">
-            <div class="l-full">
-                <h2 class="m-section--hed mm-big">Faq / Znalostní báze</h2>
-            </div>
-        </header>
-    </div>
-
-    <div class="row">
-        <?php
-
-
-
-        foreach ($node->field_segment_faq['und'] AS $faq) {
-            ?>
-            <div class="m-card_faq l-single">
-
-                <article class="m-story">
-                    <header>
-                        <h1 class="m-item--hed">
-                            <a href="<?= $GLOBALS['base_url'] ?>/node/<?= $faq['entity']->nid ?>">
-                                <?= $faq['entity']->title ?>
-                            </a>
-                        </h1>
-
-                        <div class="m-item--summary">
-                            <div class="m-item--description">
-                                <p><?= $faq['entity']->field_knowledgebase_perex[$node->language][0]['value'] ?></p>
-                            </div>
-                        </div>
-                    </header>
-                </article>
-
-            </div>
-        <?php
-        }
-        ?>
-    </div>
-
-
-    <div class="row">
-        <footer class="m-section--footer">
-            <div class="l-half">
-                <div class="m-section--top"><a href="">Nahoru <i class="fa fa-arrow-up"></i></a></div>
-            </div>
-            <div class="l-half">
-                <ul class="m-section--nav inline-right">
-                    <li><a href="" title="Novinky">Technická pomoc</a></li>
-                    <li><a href="">informace pro architekty</a></li>
-                </ul>
-            </div>
-        </footer>
-    </div>
-
-</div>
-<?php
-}
-?>
 
 
 

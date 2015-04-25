3643f7904264b7ecd94611252ad1cbbbf9eec7a5
reznimic <mich.reznicek@gmail.com>
2015-04-25 16:20:39 +0200

    Filtr

--- templates/views/views-exposed-form--zpravodaj.tpl.php
+++ templates/views/views-exposed-form--zpravodaj.tpl.php
@@ -18,8 +18,6 @@
  *
  * @ingroup views_templates
  */
-
-
 ?>
 <?php if (!empty($q)): ?>
     <?php
@@ -28,127 +26,55 @@
     print $q;
     ?>
 <?php endif; ?>
-
-<div class="m-filter">
-    <div class="l-grid">
-
-        <div class="l-left">
-
-            <ul class="m-filter--items">
-                <li class="label">
-                    <div>vyber</div>
-                </li>
-                <li>
-                    <select id="edit-field-zpravodaj-kategorie-tid" name="field_zpravodaj_kategorie_tid"
-                            class="form-select">
-                        <option value="All" selected="selected">VŠE</option>
-                        <option value="26">E-bulettin</option>
-                        <option value="24">Novinky</option>
-                        <option value="25">Příklady modulární výstavby ve světě</option>
-                        <option value="120">Projekty</option>
-                    </select>
-                </li>
-            </ul>
+<div class="views-exposed-form">
+    <div class="views-exposed-widgets clearfix">
+        <?php foreach ($widgets as $id => $widget): ?>
+            <div id="<?php print $widget->id; ?>-wrapper" class="views-exposed-widget views-widget-<?php print $id; ?>">
+                <?php if (!empty($widget->label)): ?>
+                    <label for="<?php print $widget->id; ?>">
+                        <?php print $widget->label; ?>
+                    </label>
+                <?php endif; ?>
+                <?php if (!empty($widget->operator)): ?>
+                    <div class="views-operator">
+                        <?php print $widget->operator; ?>
+                    </div>
+                <?php endif; ?>
+                <div class="views-widget">
+                    <?php print $widget->widget; ?>
+                </div>
+                <?php if (!empty($widget->description)): ?>
+                    <div class="description">
+                        <?php print $widget->description; ?>
+                    </div>
+                <?php endif; ?>
+            </div>
+        <?php endforeach; ?>
+        <?php if (!empty($sort_by)): ?>
+            <div class="views-exposed-widget views-widget-sort-by">
+                <?php print $sort_by; ?>
+            </div>
+            <div class="views-exposed-widget views-widget-sort-order">
+                <?php print $sort_order; ?>
+            </div>
+        <?php endif; ?>
+        <?php if (!empty($items_per_page)): ?>
+            <div class="views-exposed-widget views-widget-per-page">
+                <?php print $items_per_page; ?>
+            </div>
+        <?php endif; ?>
+        <?php if (!empty($offset)): ?>
+            <div class="views-exposed-widget views-widget-offset">
+                <?php print $offset; ?>
+            </div>
+        <?php endif; ?>
+        <div class="views-exposed-widget views-submit-button">
+            <?php print $button; ?>
         </div>
-
-        <div class="l-right">
-            <ul class=" m-filter--items">
-                <li>
-                    <select class="date-year form-select" id="edit-field-zpravodaj-publikace-value-value-year"
-                            name="field_zpravodaj_publikace_value[value][year]">
-                        <option value="" selected="selected"> Datum</option>
-                        <option value="1995">1995</option>
-                        <option value="1996">1996</option>
-                        <option value="1997">1997</option>
-                        <option value="1998">1998</option>
-                        <option value="1999">1999</option>
-                        <option value="2000">2000</option>
-                        <option value="2001">2001</option>
-                        <option value="2002">2002</option>
-                        <option value="2003">2003</option>
-                        <option value="2004">2004</option>
-                        <option value="2005">2005</option>
-                        <option value="2006">2006</option>
-                        <option value="2007">2007</option>
-                        <option value="2008">2008</option>
-                        <option value="2009">2009</option>
-                        <option value="2010">2010</option>
-                        <option value="2011">2011</option>
-                        <option value="2012">2012</option>
-                        <option value="2013">2013</option>
-                        <option value="2014">2014</option>
-                        <option value="2015">2015</option>
-                    </select>
-                </li>
-            </ul>
-        </div>
-
-
+        <?php if (!empty($reset_button)): ?>
+            <div class="views-exposed-widget views-reset-button">
+                <?php print $reset_button; ?>
+            </div>
+        <?php endif; ?>
     </div>
 </div>
-
-
-
-<?php foreach ($widgets as $id => $widget): ?>
-
-    <?php if (!empty($widget->label)): ?>
-
-
-    <?php endif; ?>
-    <?php if (!empty($widget->operator)): ?>
-        <div class="views-operator">
-            <?php print $widget->operator; ?>
-        </div>
-    <?php endif; ?>
-
-
-    <?php
-
-
-
- //   print $widget->widget;
-    ?>
-
-
-
-    <?php if (!empty($widget->description)): ?>
-        <div class="description">
-            <?php print $widget->description; ?>
-        </div>
-    <?php endif; ?>
-
-<?php endforeach; ?>
-
-
-
-
-
-
-<?php if (!empty($sort_by)): ?>
-    <div class="views-exposed-widget views-widget-sort-by">
-        <?php print $sort_by; ?>
-    </div>
-    <div class="views-exposed-widget views-widget-sort-order">
-        <?php print $sort_order; ?>
-    </div>
-<?php endif; ?>
-<?php if (!empty($items_per_page)): ?>
-    <div class="views-exposed-widget views-widget-per-page">
-
-        <?php print $items_per_page; ?>
-    </div>
-<?php endif; ?>
-<?php if (!empty($offset)): ?>
-    <div class="views-exposed-widget views-widget-offset">
-        <?php print $offset; ?>
-    </div>
-<?php endif; ?>
-<div class="views-exposed-widget views-submit-button">
-    <?php print $button; ?>
-</div>
-<?php if (!empty($reset_button)): ?>
-    <div class="views-exposed-widget views-reset-button">
-        <?php print $reset_button; ?>
-    </div>
-<?php endif; ?>
-

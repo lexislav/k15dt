39a545bfc80ebd08d3aa82b33fd681a298380c31
reznimic <mich.reznicek@gmail.com>
2015-04-30 14:02:10 +0200

    Upravy nodu

--- templates/node/node--segment_trhu.tpl.php
+++ templates/node/node--segment_trhu.tpl.php
@@ -30,8 +30,11 @@ $polezemi = country_get_list();
                     </div>
 
                     <div class="l-full">
-                        <figure class="m-basic-page--image" style="background-image: url(<?= $GLOBALS['base_url'] ?>/sites/default/files/segment_trhu/<?= $node->field_segment_img['und'][0]['filename'] ?>)">
-                            <a href=""><img src="<?= $GLOBALS['base_url'] ?>/sites/default/files/segment_trhu/<?= $node->field_segment_img['und'][0]['filename'] ?>" alt="" /></a>
+                        <figure class="m-basic-page--image"
+                                style="background-image: url(<?= $GLOBALS['base_url'] ?>/sites/default/files/segment_trhu/<?= $node->field_segment_img['und'][0]['filename'] ?>)">
+                            <a href=""><img
+                                    src="<?= $GLOBALS['base_url'] ?>/sites/default/files/segment_trhu/<?= $node->field_segment_img['und'][0]['filename'] ?>"
+                                    alt=""/></a>
                             <figcaption>
                                 <?= $content['field_segment_img_text']['#items'][0]['value'] ?>
                             </figcaption>
@@ -80,9 +83,12 @@ $polezemi = country_get_list();
                     <article class="m-reference">
                         <header>
                             <!-- @TODO dodelat nacitani main img z fotogalerie -->
-                            <div class="m-item--image" style="background-image: url(<?= image_style_url('x310-340', $reference['entity']->field_reference_main_img['und'][0]['uri']) ?>)">
+                            <div class="m-item--image"
+                                 style="background-image: url(<?= image_style_url('x310-340', $reference['entity']->field_reference_main_img['und'][0]['uri']) ?>)">
                                 <a href="">
-                                    <img src="<?= image_style_url('x310-340', $reference['entity']->field_reference_main_img['und'][0]['uri']) ?>" alt="" />
+                                    <img
+                                        src="<?= image_style_url('x310-340', $reference['entity']->field_reference_main_img['und'][0]['uri']) ?>"
+                                        alt=""/>
                                 </a>
                             </div>
 
@@ -99,24 +105,30 @@ $polezemi = country_get_list();
                                         }
 
                                         ?>
-                                        <br />
-                                        <a href="<?= $GLOBALS['base_url'] ?>/node/<?= $reference['entity']->nid ?>">Detail reference</a> &rarr; [
-                                        <a href="<?= $GLOBALS['base_url'] ?>/node/<?= $reference['entity']->nid ?>/edit">editovat</a> ]
+                                        <br/>
+                                        <a href="<?= $GLOBALS['base_url'] ?>/node/<?= $reference['entity']->nid ?>">Detail
+                                            reference</a> &rarr; [
+                                        <a href="<?= $GLOBALS['base_url'] ?>/node/<?= $reference['entity']->nid ?>/edit">editovat</a>
+                                        ]
                                     </p>
                                 </div>
                             </div>
 
                             <div class="m-properties">
                                 <dl>
-                                    <dt>Zařazení</dt><dd><?= $polejmen[$reference['entity']->field_reference_kategorie_stavby['und'][0]['tid']]->name ?> – <?= $polejmen[$reference['entity']->field_reference_kategorie_stavby['und'][1]['tid']]->name ?></dd>
+                                    <dt>Zařazení</dt>
+                                    <dd><?= $polejmen[$reference['entity']->field_reference_kategorie_stavby['und'][0]['tid']]->name ?>
+                                        – <?= $polejmen[$reference['entity']->field_reference_kategorie_stavby['und'][1]['tid']]->name ?></dd>
                                 </dl>
 
                                 <dl>
-                                    <dt>Rok realizace</dt><dd><?= $rok ?></dd>
+                                    <dt>Rok realizace</dt>
+                                    <dd><?= $rok ?></dd>
                                 </dl>
 
                                 <dl>
-                                    <dt>Země realizace</dt><dd><?= $polezemi[$reference['entity']->field_reference_zeme['und'][0]['iso2']] ?></dd>
+                                    <dt>Země realizace</dt>
+                                    <dd><?= $polezemi[$reference['entity']->field_reference_zeme['und'][0]['iso2']] ?></dd>
                                 </dl>
 
                                 <!--@TODO dodelat if pro zobrazeni-->
@@ -140,26 +152,101 @@ $polezemi = country_get_list();
 
                                     <?php if (isset($reference['entity']->field_reference_plocha['und'][0]['value'])) { ?>
                                         <dt class="mm-sub">užitná plocha</dt>
-                                        <dd><?= $reference['entity']->field_reference_plocha['und'][0]['value'] ?> m<sup>2</sup></dd>
+                                        <dd><?= $reference['entity']->field_reference_plocha['und'][0]['value'] ?>
+                                            m<sup>2</sup>
+                                        </dd>
                                     <?php } ?>
                                 </dl>
+
+
                             </div>
+                        </header>
+                    </article>
+                </div>
+            <?php } ?>
+        </div>
+    </div>
+
+
+
+<?php
+$block = module_invoke('views', 'block_view', 'segmentreference-block');
+print render($block);
+?>
+
+<?php if (isset($node->field_segment_reference['und'])) { ?>
+    <div class="m-section l-feed_list bg-white">
+        <div class="row">
+            <?php
+            $poletid = array();
+            foreach ($node->field_segment_reference['und'] AS $referencetemp) {
+                $poletid[] = ($referencetemp['entity']->field_reference_kategorie_stavby['und'][0]['tid']);
+                if (isset($referencetemp['entity']->field_reference_kategorie_stavby['und'][1]['tid'])) {
+                    $poletid[] = ($referencetemp['entity']->field_reference_kategorie_stavby['und'][1]['tid']);
+                }
+            }
+            $polejmen = taxonomy_term_load_multiple($poletid);
+
+            foreach ($node->field_segment_reference['und'] AS $reference) {
+
+
+                $reference['entity']->field_reference_zeme['und'][0]['iso2'];
+
+
+                $rok = str_replace('-01-01 00:00:00', '', $reference['entity']->field_reference_rok['und'][0]['value']);
+                ?>
+                <div class="m-card_reference-horizontal l-single">
+                    <article class="m-reference ">
+                        <header>
+                            <!--                        @TODO dodelat nacitani main img z fotogalerie-->
+                            <div class="m-item--image"
+                                 style="background-image: url(<?= image_style_url('x310-340', $reference['entity']->field_reference_main_img['und'][0]['uri']) ?>)">
+                                <a href="">
+                                    <img
+                                        src="<?= image_style_url('x310-340', $reference['entity']->field_reference_main_img['und'][0]['uri']) ?>"
+                                        alt=""/>
+                                </a>
+                            </div>
+
+                            <div class="m-item--summary">
+                                <h1 class="m-item--hed">
+                                    <a href="<?= $GLOBALS['base_url'] ?>/node/<?= $reference['entity']->nid ?>"><?= $reference['entity']->title ?></a>
+                                </h1>
+
+                                <div class="m-item--description">
+                                    <?php
+
+                                    ?>
+
+
+                                    <p><?php if (isset($reference['entity']->field_reference_perex[$node->language][0]['value'])) {
+                                            echo $reference['entity']->field_reference_perex[$node->language][0]['value'];
+                                        } else {
+                                            echo truncate($reference['entity']->field_reference_text[$node->language][0]['value']);
+                                        }
+
+                                        ?>
+                                        <br/>
+                                        <a href="<?= $GLOBALS['base_url'] ?>/node/<?= $reference['entity']->nid ?>">Detail
+                                            reference</a> &rarr; [ <a
+                                            href="<?= $GLOBALS['base_url'] ?>/node/<?= $reference['entity']->nid ?>/edit">editovat</a>
+                                        ]</p>
+
+                                </div>
                         </header>
                     </article>
 
                 </div>
             <?php } ?>
 
+
         </div>
     </div>
 
-
 <?php
-    $block = module_invoke('views', 'block_view', 'segmentreference-block');
-    print render($block);
+}
+
 ?>
-
-
 
 
 <?php

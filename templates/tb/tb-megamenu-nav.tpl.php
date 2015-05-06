3530331c1c18f7857251ddcbc5003371807ec300
Michal Hotovec <michal.hotovec@gmail.com>
2015-05-06 23:36:37 +0200

    hamburger, mega menu A,B

--- templates/tb/tb-megamenu-nav.tpl.php
+++ templates/tb/tb-megamenu-nav.tpl.php
@@ -1,3 +1,6 @@
-<div class="nav">
+<div class="nav desktop">
   <?php print $lis;?>
+</div>
+<div class="nav mobile">
+  <div class="item mega-mobile tb-megamenu-item level-1 mega" open-mega-mobile><i class="fa fa-bars"></i></div>
 </div>

adfbb0fecefd3436f4371cc039ed025730a45aae
reznimic <mich.reznicek@gmail.com>
2015-05-06 16:09:31 +0200

    telefon jen pro EN

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -3,10 +3,12 @@
 
 
 <?php
-if($user->uid==1){
+global $language;
+if ($user->uid == 1) {
     print $messages;
+
 }
- ?>
+?>
 
 
 <div class="l-navbar">
@@ -59,12 +61,17 @@ if($user->uid==1){
             //    print render($page['header']); ?>
 
 
-
-            <ul class="m-navbar--menu m-navbar--menu-primary">
-                <li class="m-navbar--menu-callout">
-                    <span class="color-primary"><?php print t('Call us') ?>:</span> <?php print t('(+421) 20 577 007 711') ?>
-                </li>
-            </ul>
+            <?php
+            //            @TODO udelat editovatelne, az bude cas
+            if ($language->language == 'en') {
+                ?>
+                <ul class="m-navbar--menu m-navbar--menu-primary">
+                    <li class="m-navbar--menu-callout">
+                        <span class="color-primary"><?php print t('Call us') ?>
+                            :</span> <?php print t('(+421) 20 577 007 711') ?>
+                    </li>
+                </ul>
+            <?php } ?>
 
         </div>
     </div>
@@ -72,142 +79,150 @@ if($user->uid==1){
     <div class="m-navbar mm-primary" id="navbarPrimary">
 
 
-
         <div class="m-navbar--container">
 
             <a class="m-navbar--logo" href="/">
-                <div class="mm-has-svg" style="background-image: url('/sites/koma/assets/images/logo-koma-modular.svg');"></div>
+                <div class="mm-has-svg"
+                     style="background-image: url('/sites/koma/assets/images/logo-koma-modular.svg');"></div>
             </a>
 
             <div class="xmega">
                 <?php print render($page['navigation']); ?>
-<!--            </div>-->
-<!--            --><?//= $zip_code = db_query("SELECT field_megamenu_html_value from {field_data_field_megamenu_html} ")->fetchField(); ?>
-        </div>
-    </div>
-
-</div>
-<?php print render($page['hledani']); ?>
-<?php print render($page['content']); ?>
-
-
-<!--certifikace-->
-<div class="m-section l-feed_six">
-    <header class="m-section--header">
-        <h2 class="m-section--hed mm-small mm-center color-secondary"><?php print t('Certifikace') ?></h2>
-    </header>
-    <div class="row rowfix">
-        <?php
-        $block = module_invoke('views', 'block_view', 'certifik_ty-block');
-        print render($block);
-        ?>
-    </div>
-    <div class="row">
-        <footer class="m-section--footer"></footer>
-    </div>
-</div><!--certifikace-->
-
-
-<!--support-->
-<div class="m-section m-section_support">
-
-    <div class="l-grid">
-        <div class="m-section--item">
-
-            <article class="m-newsletter">
-                <header>
-                    <div class="m-item--hed-icon" style="background-image: url(<?= $GLOBALS['base_url'] ?>/sites/all/themes/koma/assets/images/votaznik.png)"></div>
-                    <h1 class="m-item--hed mm-small color-primary"><?php print t('Podpora pro zákazníky') ?></h1>
-                </header>
-
-                <div class="m-item--description"><?php print t('Nenašli jste zde odpověď na Vaše otázky? Zeptejte se nás přímo!') ?></div>
-                <button><?php print t('Zeptejte se nás') ?> &rarr;</button>
-            </article>
-
+                <!--            </div>-->
+                <!--            --><? //= $zip_code = db_query("SELECT field_megamenu_html_value from {field_data_field_megamenu_html} ")->fetchField(); ?>
+            </div>
         </div>
 
-        <div class="m-section--item worker">
-            <img src="<?= $GLOBALS['base_url'] ?>/sites/koma/assets/images/worker.jpg" />
-        </div>
-
-        <div class="m-section--item">
-
-            <article class="m-bulletin">
-                <header>
-                    <h1 class="m-item--hed mm-small color-primary"><?php print t('Podpora pro zákazníky') ?></h1>
-                </header>
-
-                <?php print render($page['form_email']); ?>
-
-                <div class="m-item--description"><?php print t('Odběrem našeho měsíčního ebulletinu získáte přehled o dění ve světě modulární architektury i o novinkách systému modulární výstavby KOMA.') ?></div>
-
-            </article>
-
-        </div>
     </div>
-</div><!--support-->
+    <?php print render($page['hledani']); ?>
+    <?php print render($page['content']); ?>
 
-<!--paticka-->
-<footer class="m-footer m-section l-footer">
-    <div class="m-footer--container">
+
+    <!--certifikace-->
+    <div class="m-section l-feed_six">
+        <header class="m-section--header">
+            <h2 class="m-section--hed mm-small mm-center color-secondary"><?php print t('Certifikace') ?></h2>
+        </header>
+        <div class="row rowfix">
+            <?php
+            $block = module_invoke('views', 'block_view', 'certifik_ty-block');
+            print render($block);
+            ?>
+        </div>
         <div class="row">
-
-
-            <section class="m-footer--section">
-                <a class="m-footer--logo" href="/">
-                    <img src="<?= $GLOBALS['base_url'] ?>/sites/all/themes/koma/assets/images/logo-koma-modular-inline-white.svg" alt="" />
-                </a>
-
-            </section>
-
-
-            <section class="m-footer--section">
-
-
-                <?php print theme('links__system_secondary_menu', array(
-                    'links' => $secondary_menu,
-                    'attributes' => array(
-                        'class' => array('m-footer--menu-primary'),
-                    )
-                )); ?>
-                <!--            @TODO Upravit podle predlohy-->
-                <!--                    <li class="m-footer--menu-link">-->
-                <!--                        <a target="{{ link.target }}" title="{{ link.title }}"><em class="">{{ link.title }}</em></a>-->
-                <!--                    </li>-->
-            </section>
-
-            <section class="m-footer--section">
-                <ul class="m-footer--menu-secondary">
-
-                    <li class="m-footer--menu-link">
-                        <a href="" target="" title=""><i class="fa fa-facebook"></i></a>
-                    </li>
-                    <li class="m-footer--menu-link">
-                        <a href="" target="" title=""><i class="fa fa-twitter"></i></a>
-                    </li>
-                    <li class="m-footer--menu-link">
-                        <a href="" target="" title=""><i class="fa fa-youtube-play"></i></a>
-                    </li>
-                </ul>
-
-            </section>
+            <footer class="m-section--footer"></footer>
         </div>
-
-        <div class="row">
-
-            <section class="m-footer--subsection">
-                <a class="m-footer--author" href="/">
-                    <a href="">© 2015 – 2017 KOMA Modular, s. r. o.</a>
-                </a>
-            </section>
-
-            <section class="m-footer--subsection">
-                <a class="m-footer--author" href="/">
-                    <a href="">Od Oka</a>
-                </a>
-            </section>
-        </div>
-
-
     </div>
-</footer><!--paticka-->
+    <!--certifikace-->
+
+
+    <!--support-->
+    <div class="m-section m-section_support">
+
+        <div class="l-grid">
+            <div class="m-section--item">
+
+                <article class="m-newsletter">
+                    <header>
+                        <div class="m-item--hed-icon"
+                             style="background-image: url(<?= $GLOBALS['base_url'] ?>/sites/all/themes/koma/assets/images/votaznik.png)"></div>
+                        <h1 class="m-item--hed mm-small color-primary"><?php print t('Podpora pro zákazníky') ?></h1>
+                    </header>
+
+                    <div
+                        class="m-item--description"><?php print t('Nenašli jste zde odpověď na Vaše otázky? Zeptejte se nás přímo!') ?></div>
+                    <button><?php print t('Zeptejte se nás') ?> &rarr;</button>
+                </article>
+
+            </div>
+
+            <div class="m-section--item worker">
+                <img src="<?= $GLOBALS['base_url'] ?>/sites/koma/assets/images/worker.jpg"/>
+            </div>
+
+            <div class="m-section--item">
+
+                <article class="m-bulletin">
+                    <header>
+                        <h1 class="m-item--hed mm-small color-primary"><?php print t('Podpora pro zákazníky') ?></h1>
+                    </header>
+
+                    <?php print render($page['form_email']); ?>
+
+                    <div
+                        class="m-item--description"><?php print t('Odběrem našeho měsíčního ebulletinu získáte přehled o dění ve světě modulární architektury i o novinkách systému modulární výstavby KOMA.') ?></div>
+
+                </article>
+
+            </div>
+        </div>
+    </div>
+    <!--support-->
+
+    <!--paticka-->
+    <footer class="m-footer m-section l-footer">
+        <div class="m-footer--container">
+            <div class="row">
+
+
+                <section class="m-footer--section">
+                    <a class="m-footer--logo" href="/">
+                        <img
+                            src="<?= $GLOBALS['base_url'] ?>/sites/all/themes/koma/assets/images/logo-koma-modular-inline-white.svg"
+                            alt=""/>
+                    </a>
+
+                </section>
+
+
+                <section class="m-footer--section">
+
+
+                    <?php print theme('links__system_secondary_menu', array(
+                        'links' => $secondary_menu,
+                        'attributes' => array(
+                            'class' => array('m-footer--menu-primary'),
+                        )
+                    )); ?>
+                    <!--            @TODO Upravit podle predlohy-->
+                    <!--                    <li class="m-footer--menu-link">-->
+                    <!--                        <a target="{{ link.target }}" title="{{ link.title }}"><em class="">{{ link.title }}</em></a>-->
+                    <!--                    </li>-->
+                </section>
+
+                <section class="m-footer--section">
+                    <ul class="m-footer--menu-secondary">
+
+                        <li class="m-footer--menu-link">
+                            <a href="" target="" title=""><i class="fa fa-facebook"></i></a>
+                        </li>
+                        <li class="m-footer--menu-link">
+                            <a href="" target="" title=""><i class="fa fa-twitter"></i></a>
+                        </li>
+                        <li class="m-footer--menu-link">
+                            <a href="" target="" title=""><i class="fa fa-youtube-play"></i></a>
+                        </li>
+                    </ul>
+
+                </section>
+            </div>
+
+            <div class="row">
+
+                <section class="m-footer--subsection">
+                    <a class="m-footer--author" href="/">
+                        <a href="">© 2015 – 2017 KOMA Modular, s. r. o.</a>
+                    </a>
+                </section>
+
+                <section class="m-footer--subsection">
+                    <a class="m-footer--author" href="/">
+                        <a href="">Od Oka</a>
+                    </a>
+                </section>
+            </div>
+
+
+        </div>
+    </footer>
+    <!--paticka-->

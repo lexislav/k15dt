1c666ea925ea7925cd9aad1ec9536af160a7e6f1
Michal Hotovec <michal.hotovec@gmail.com>
2015-05-07 17:14:31 +0200

    mezera mezi drobkama

--- templates/node/node--odkazy-publicita.tpl.php
+++ templates/node/node--odkazy-publicita.tpl.php
@@ -1,30 +1,28 @@
 <div class="m-section l-detail-page">
     <div class="row">
         <header class="m-section--header">
-<!--            <div class="l-full">-->
-<!--                <nav class="breadcrumbs">-->
-<!--                    <a href="#">Home</a>-->
-<!--                    <a href="#">Features</a>-->
-<!--                    <a class="unavailable" href="#">Gene Splicing</a>-->
-<!--                    <a class="current" href="#">Cloning</a>-->
-<!--                </nav>-->
-<!---->
-<!--            </div>-->
+            <!--            <div class="l-full">-->
+            <!--                <nav class="breadcrumbs">-->
+            <!--                    <a href="#">Home</a>-->
+            <!--                    <a href="#">Features</a>-->
+            <!--                    <a class="unavailable" href="#">Gene Splicing</a>-->
+            <!--                    <a class="current" href="#">Cloning</a>-->
+            <!--                </nav>-->
+            <!---->
+            <!--            </div>-->
         </header>
     </div>
-<!--    --><?php
+    <!--    --><?php
 
 
-//    ?>
+    //    ?>
     <article class="m-reference">
         <header>
-            <div class="m-reference--image mm-preview-fit"  >
-
-            </div>
+            <div class="m-reference--image mm-preview-fit"></div>
             <div class="m-reference--summary">
                 <h1 class="m-reference--hed"><a href=""><?php print $title; ?></a></h1>
-                <div class="m-reference--description">
-                </div>
+
+                <div class="m-reference--description"></div>
                 <div class="m-reference--meta m-properties">
                     <dl>
                         <dt><?php print t('Zařazení publicity') ?></dt>
@@ -32,11 +30,12 @@
                     </dl>
                     <dl>
                         <dt><?php print t('Odkaz') ?></dt>
-                        <dd><a href="<?= ($content['field_odkazy_p_link']['#items'][0]['display_url']); ?>"><?=$title?></a> </dd>
+                        <dd><a href="<?= ($content['field_odkazy_p_link']['#items'][0]['display_url']); ?>"><?= $title ?></a>
+                        </dd>
                     </dl>
                     <dl>
                         <dt><?php print t('Popis publicity') ?></dt>
-                        <dd><?=$content['field_odkazy_p_popisek']['#items'][0]['value']?></dd>
+                        <dd><?= $content['field_odkazy_p_popisek']['#items'][0]['value'] ?></dd>
                     </dl>
 
                 </div>

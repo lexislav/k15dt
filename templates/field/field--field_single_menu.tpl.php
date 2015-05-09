345aa359cf050943d3335a048faa8737d7c88e2f
reznimic <mich.reznicek@gmail.com>
2015-05-09 17:30:07 +0200

    jazyk

--- templates/field/field--field_single_menu.tpl.php
+++ templates/field/field--field_single_menu.tpl.php
@@ -3,12 +3,16 @@
     <?php
     foreach ($items AS $poradi => $pole) {
         if ($poradi == 0) continue;
-//        dpm($pole);
-//        dpm($pole['field_page_link']['#object']);
+        global $language;
+        if($language->language=='cs'){
+            $jazyk = '';
+        }else{
+            $jazyk = $language->language.'/';
+        }
 
         ?>
         <li class="text-double-button">
-            <a href="<?= @ $pole['field_page_link']['#items'][0]['url'] ?>"><?= @ $pole['field_basic_title']['#items'][0]['value'] ?>
+            <a href="<?=@drupal_get_path_alias($jazyk.$pole['field_page_link']['#items'][0]['url']) ?>"><?= @ $pole['field_basic_title']['#items'][0]['value'] ?>
                 <span><?= $pole['field_basic_popis']['#items'][0]['value'] ?></span></a></li>
     <?php } ?>
 </ul>

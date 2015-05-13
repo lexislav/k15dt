e1ba3dd132044da3d944588b7b8d8fdcb1250f04
reznimic <mich.reznicek@gmail.com>
2015-05-13 13:16:57 +0200

    zmena odkazu

--- templates/field/field--field_single_menu.tpl.php
+++ templates/field/field--field_single_menu.tpl.php
@@ -5,14 +5,14 @@
         if ($poradi == 0) continue;
         global $language;
         if($language->language=='cs'){
-            $jazyk = '';
+            $jazyk = '/';
         }else{
-            $jazyk = $language->language.'/';
+            $jazyk = '/'.$language->language.'/';
         }
 
         ?>
         <li class="text-double-button">
-            <a href="<?=@drupal_get_path_alias($jazyk.$pole['field_page_link']['#items'][0]['url']) ?>"><?= @ $pole['field_basic_title']['#items'][0]['value'] ?>
+            <a href="<?=@($GLOBALS['base_url'].$jazyk.$pole['field_page_link']['#items'][0]['url']) ?>"><?= @ $pole['field_basic_title']['#items'][0]['value'] ?>
                 <span><?= $pole['field_basic_popis']['#items'][0]['value'] ?></span></a></li>
     <?php } ?>
 </ul>
